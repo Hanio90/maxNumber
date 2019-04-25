764636eca27da31a3e4c1ac66c2d45e562c0405c
Hani Osman <hanio90@gmail.com>
2019-04-24 21:26:53 -0400

    just some extra stuff

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const util = require('./maxNumber');
 
 let maxValue = util.maxNumber(1230, 999);
 console.log(maxValue);
+maxValue = util.maxNumber(90, 290231);
+console.log(maxValue);
+maxValue = util.maxNumber(3172893, 873478);
+console.log(maxValue);
